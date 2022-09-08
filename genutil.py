38f9699650ca0d6b3fb4fb4339baadfedb13c867
Nathaniel Gibson <nathaniel.gibson@gmail.com>
2022-09-08 14:04:00 -0500

    fix of a fix ... christ

--- genutil.py
+++ genutil.py
@@ -64,7 +64,7 @@ def load_image(file_path):
 		return None
 
 # Image2Image Generates and saves multiple images based on an input image or list of images and a list of seeds
-def generate(image_path: str, prompt: PromptGenerator, strength=0.5, seeds=None, num_seeds=10, fileid=None, filename=None, guidance_scale=7.5, ext='jpg', overwrite_strength=False, images=None, output_folder='_out/', verbose=False, device="cuda"):
+def generate(image_path: str, prompt: PromptGenerator, strength=0.5, seeds=None, num_seeds=10, fileid=None, filename=None, guidance_scale=7.5, ext='jpg', overwrite_strength=False, images=None, output_folder='_out/', verbose=False):
 	global device, pipe, scheduler
 	if seeds is None:
 		seeds = sample(range(0, 999999999), num_seeds)
